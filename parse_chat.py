1b70b45996c154a624ed3d5bb9ef3dc9d409c9b4
Sarztak <sar3142168@gmail.com>
2026-01-11 18:50:14 -0600

    added support for recursive parsing inside the element but still need to improve on it, it still works for now

--- parse_chat.py
+++ parse_chat.py
@@ -1,4 +1,5 @@
-from bs4 import BeautifulSoup
+from rich.traceback import install; install()
+from bs4 import BeautifulSoup, NavigableString
 import re 
 
 def escape_latex_text(s):
@@ -26,6 +27,8 @@ def clean_ele_text(ele, escape=True):
 
 def process_element(element):
     result = []
+    if isinstance(element, NavigableString):
+        return element.string
     for ele in element.children:
         if ele.name == 'strong':
             text = clean_ele_text(ele) 
@@ -33,9 +36,6 @@ def process_element(element):
         elif ele.name in ('em', 'i'):
             text = clean_ele_text(ele) 
             latex = f"\\textit{{{text}}}"
-        elif ele.name == 'code':
-            text = clean_ele_text(ele, escape=False) 
-            latex = f"\\texttt{{{text}}}"
         elif ele.name == 'a':
             href = ele.get('href', '')
             text = clean_ele_text(ele)
@@ -44,15 +44,54 @@ def process_element(element):
             latex = clean_ele_text(ele)
         result.append(latex)
 
+    return "\n".join(result)
+
+def process_list(element, ordered=False):
+    _type = "enumerate" if ordered else "itemize"
+    li_list = [f"\begin{{{_type}}}"]
+    if isinstance(element, NavigableString):
+        return element.string
+    for li in element.children:
+        # process each li element
+        text = process_element(li)  
+        item_text = f"\item {text}" 
+        li_list.append(item_text) 
+    # append begin itemize end itemize
+    li_list.append(f"\end{{{_type}}}")
+    li_block = "\n".join(li_list)
+    return li_block
+
 def process_div_children(div):
+    # breakpoint()
+    text_block = []
+    if isinstance(div, NavigableString):
+        return div.string
     for child in div.children:
+        if child.name == 'div':
+            div_text = process_div_children(child)
+            text_block.append(div_text)
         if child.name == 'p':
             p_text = process_element(child) 
+            text_block.append(p_text)
         elif child.name == 'ul':
-            # append begin itemize end itemize
-            pass
+            # get all the immediate children which are li elements
+            ul_block = process_list(child, ordered=False)
+            text_block.append(ul_block)
         elif child.name == 'ol':
+            ol_block = process_list(child, ordered=True)
+            text_block.append(ol_block)
+        elif child.name == 'code': 
+            breakpoint()
+            text = clean_ele_text(child, escape=False) 
+            latex = f"\\texttt{{{text}}}"
+            text_block.append(latex)
             pass
+        else:
+            plain_text = clean_ele_text(child) 
+            text_block.append(plain_text)
+
+    return "\n".join(text_block)
+
 def main():
     with open('claude_stripped_down_chat.html', 'r', encoding='utf8') as fp:
         html_parser = BeautifulSoup(fp, 'html.parser',multi_valued_attributes=None)
@@ -61,9 +100,10 @@ def main():
     user_messages = []
     for div in divs:
         if div.get('data-testid', "") == 'user-message':
-            pass
+            div_text = process_div_children(div)
         elif div.get('class', "").startswith('font-claude-message'):
-            breakpoint()
+            div_text = process_div_children(div)
+            pass
 
 if __name__ == "__main__":
     main()
