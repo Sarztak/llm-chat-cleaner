142c3f97336947ce15c1fed6ea4f834222ec9ced
Sarztak <sar3142168@gmail.com>
2026-01-25 17:26:58 -0600

    writing a basic parser to convert from markdown to latex, it cannot handle everything, but I still want to build it; current stuck on how to handle unordered lists

--- markdown_to_tex.py
+++ markdown_to_tex.py
@@ -1,10 +1,39 @@
 import re
+from collections import deque
 import markdown
 # open the file first
 user_pattern = 'User prompt'
 bot_pattern = 'GPT-4o mini'
 pattern = rf'{user_pattern}|{bot_pattern}'
 paragraphs = dict(user=[], bot=[])
+
+
+def capture_ordered_list(text):
+    pattern = r"""^(\d+)\.\s(.*)"""
+    matches = re.findall(pattern, text, re.MULTILINE)
+    queue = deque(matches)
+    lists = []
+    item_list = []
+    if queue: # check if the matches is empty or not
+        num, item = queue.popleft() # the first match should be 1. 
+        if num == 1:
+            item_list.append(item) # add the first match and then start poping
+        while queue:
+            num, item = queue.popleft()
+            if num == 1: # this means we have a new ordered list
+                lists.append(item_list) # add to the collection of lists
+                item_list = [] # start a new list
+                item_list.append(item)
+            else:
+                item_list.append(item) # add the next item in sequence
+
+    # lists should now contain all the ordered lists
+
+def capture_unordered_list(text):
+    pattern = r"""^-\s(.*)"""
+    matches = re.findall(pattern, text, re.MULTILINE)
+
+    # now this is going to be trickly because there is no delimiter
 
 def collect_line(fp, role):
     para = []
@@ -35,8 +64,7 @@ with open('assorted 1.md', 'r', encoding='utf8') as fp:
 if __name__ == "__main__":
     with open('assorted 1.md', 'r', encoding='utf8') as fp:
         lines = fp.readlines()
-    breakpoint()
     markdown_text = "".join(lines)
-    html = markdown.markdown(markdown_text, extensions=['fenced_code', 'codehilite'])
+    html = markdown.markdown(markdown_text, extensions=['fenced_code'])
     with open('assorted_1.html', 'w', encoding='utf8') as wp:
         wp.write(html)
