9105441079a859cbf7ce1bc125c32a6554cf19a8
Sarztak <sar3142168@gmail.com>
2026-01-29 17:30:10 -0600

    I wanted to try something out to capture things in between the elements in the ordered lists in case there is something other than newlines separating them,and it looks like I was able to. But the same cannot be done for unordered lists because there is nothing to stop in between. And even for ordered list this may just be overkill or maybe not. In any case I learned a lot

--- md_to_html.py
+++ md_to_html.py
@@ -7,9 +7,37 @@ if __name__ == "__main__":
     markdown_text = "".join(lines)
     code_pattern = r"""^\s*```([\s\S]*?)```$"""
     ol_pattern = r"""^(\d+\.\s.*(?:\n*\d+\.\s.*)*)"""
+    test_ol_pattern = re.compile(r"""^(\d+)\.\s(.*)""", re.M)
     ul_pattern = r"""^(-\s.*(?:\n*-\s.*)*)*"""
-    splits = re.split(code_pattern, markdown_text, flags=re.MULTILINE)    
-    breakpoint()
+    ol_elements = []
+    prev_match1 = -1
+    prev_match2 = ""
+    running_text = ""
+
+    for m in test_ol_pattern.finditer(markdown_text):
+        curr_match1 = m.group(1)
+        curr_match2 = m.group(2)
+
+        if int(curr_match1) == 1 and running_text:
+            # start of a new capture group
+            # store the previous one
+            ol_elements.append(running_text) 
+            running_text = f"{curr_match1}. {curr_match2}" 
+        elif int(prev_match1) + 1 == int(curr_match1):
+            # expand the text and update the match
+            regex_pattern = rf"""{re.escape(prev_match1)}\.\s{re.escape(prev_match2)}([\s\S]*?){re.escape(curr_match1)}\.\s{re.escape(curr_match2)}"""
+            pattern = re.compile(regex_pattern, re.M)
+            match = pattern.search(markdown_text)
+            running_text = running_text + match.group(1) + f"{curr_match1}. {curr_match2}" 
+        elif int(prev_match1) == -1:
+            running_text = f"{curr_match1}. {curr_match2}" # for the first list we need to start with something
+        
+        prev_match1 = curr_match1
+        prev_match2 = curr_match2
+    with open('md_to_html_op.txt', 'w', encoding='utf8') as w:
+        for line in ol_elements:
+            w.write(line + '\n\n-----------------------------------------\n\n')
+    # splits = re.split(code_pattern, markdown_text, flags=re.MULTILINE)    
     # html = markdown.markdown(markdown_text, extensions=['fenced_code'])
     # with open('assorted_1.html', 'w', encoding='utf8') as wp:
     #     wp.write(html)
